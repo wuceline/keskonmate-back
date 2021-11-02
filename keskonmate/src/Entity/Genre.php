e4cfc2af926c6e86fc813a16e289f13a023270a0
Tux87 <edward.juniper87@gmail.com>
2021-11-02 12:49:07 +0100

    Added Use UniqueEntity to genre entity

--- keskonmate/src/Entity/Genre.php
+++ keskonmate/src/Entity/Genre.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=GenreRepository::class)
