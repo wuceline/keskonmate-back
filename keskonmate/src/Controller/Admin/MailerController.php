96cc2f91072cd51b688778f4270b9a1ef7ee4362
Tux87 <edward.juniper87@gmail.com>
2021-11-04 16:11:01 +0100

    Test sending first emails OK

--- keskonmate/src/Controller/Admin/MailerController.php
+++ keskonmate/src/Controller/Admin/MailerController.php
@@ -4,17 +4,30 @@ namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class MailerController extends AbstractController
 {
     /**
-     * @Route("/admin/mailer", name="admin_mailer")
+     * @Route("/email")
      */
-    public function index(): Response
+    public function sendEmail(MailerInterface $mailer): Response
     {
-        return $this->render('admin/mailer/index.html.twig', [
-            'controller_name' => 'MailerController',
-        ]);
+        $email = (new Email())
+            ->from('keskonmate@gmail.com')
+            ->to('keskonmate@gmail.com')
+            // ->cc('alexisbertell@gmail.com', 'dorian.lalanne@yahoo.fr', 'wuwuceline@gmail.com', 'eddyzsoma@gmail.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('COUCOU!!!!!');
+
+        $mailer->send($email);
+        return 
     }
 }
